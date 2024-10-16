75e6e40d1b6e9a67ef5980ed6dcaf7792cebd474
rjsanjaymandal <mandalcreator@gmail.com>
2024-10-16 17:19:34 +0530

    s

---
+++ src/components/UserCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+        <p id='title'>Sanjay Mandal</p>
+        <img id='user-img' src='' alt='sanjay'></img>
+        <p id='user-dec'>Description of Sanjay Mandal</p>
+    </div>
+  )
+}
+
+export default UserCard
